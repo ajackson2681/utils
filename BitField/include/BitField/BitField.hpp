a76d2c2c50c08115e1d3325c84b0c0df2b8a522b
Adam Jackson <ajackson2681@gmail.com>
2026-07-15 18:24:28 -0500

    Add extra functionality to BitField
    add overloaded operators so you can call stuff on the bitfield object directly. Like for instance, instead of
    
    bf.SetBit(0, 1);
    you can do
    bf[0] = 1;
    
    Or instead of
    bf.SetBits(3, 0, 0b1111);
    you can do
    bf(3, 0) = 0b1111;
    
    The exposed methods are still there already, but this just adds some extra flexibility and code preference.

--- BitField/include/BitField/BitField.hpp
+++ BitField/include/BitField/BitField.hpp
@@ -15,6 +15,8 @@ struct FieldSpec
 template <typename T>
 class BitField 
 {
+    class BitFieldProxy;
+
 public:
     BitField(T initial)
         : raw(initial)
@@ -35,6 +37,36 @@ public:
     operator T() const 
     {
         return raw;
+    }
+
+    T operator()(int highBit, int lowBit) const 
+    {
+        return GetBits(highBit, lowBit);
+    }
+
+    T operator()(const FieldSpec& spec) const 
+    {
+        return Get(spec);
+    }
+
+    BitFieldProxy operator()(int highBit, int lowBit)
+    {
+        return BitFieldProxy(*this, highBit, lowBit);
+    }
+
+    BitFieldProxy operator()(const FieldSpec& spec) 
+    {
+        return BitFieldProxy(*this, spec.highBit, spec.lowBit);
+    }
+
+    T operator[](int bitPosition) const 
+    {
+        return GetBit(bitPosition);
+    }
+
+    BitFieldProxy operator[](int bitPosition)
+    {
+        return BitFieldProxy(*this, bitPosition, bitPosition);
     }
 
     void SetBit(int bitPosition, bool value) 
@@ -103,4 +135,40 @@ public:
 
 private:
     T raw;  
+
+    /**
+     * Proxy returned by the non-const call operators so that a field
+     * selection can appear on the left-hand side of an assignment.
+     * Reading converts to T; assigning writes back into the parent field.
+     */
+    class BitFieldProxy
+    {
+    public:
+        BitFieldProxy(BitField& field, int highBit, int lowBit)
+            : field(field)
+            , highBit(highBit)
+            , lowBit(lowBit)
+        {}
+
+        operator T() const
+        {
+            return field.GetBits(highBit, lowBit);
+        }
+
+        BitFieldProxy& operator=(T value)
+        {
+            field.SetBits(highBit, lowBit, value);
+            return *this;
+        }
+
+        BitFieldProxy& operator=(const BitFieldProxy& other)
+        {
+            return *this = static_cast<T>(other);
+        }
+
+    private:
+        BitField& field;
+        int highBit;
+        int lowBit;
+    };
 };
